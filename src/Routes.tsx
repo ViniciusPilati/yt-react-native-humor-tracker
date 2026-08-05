28cb9963cf0010a02f6dc484c3596a025e6bdc08
ViniciusPilati <vini1718a@gmail.com>
2026-08-05 20:18:32 -0300

    feat: configura biome para linter

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,31 +1,29 @@
-import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Text } from "react-native"
-import { HomePage } from "./screens/Home";
-import { createStaticNavigation, RouteProp } from "@react-navigation/native";
+import { createStaticNavigation, type RouteProp } from "@react-navigation/native";
+import { createNativeStackNavigator, type NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { DetailPage } from "./screens/Detail";
+import { HomePage } from "./screens/Home";
 import { SetUserName } from "./screens/SetUserName";
 
 type TScreenDefinitions = {
-  home: undefined;
-  detail: { rate: number };
-  setUserName: undefined;
-}
+	home: undefined;
+	detail: { rate: number };
+	setUserName: undefined;
+};
 
 const RootStack = createNativeStackNavigator<TScreenDefinitions>({
-  screens: {
-    home: HomePage,
-    detail: DetailPage,
-    setUserName: SetUserName,
-  },
+	screens: {
+		home: HomePage,
+		detail: DetailPage,
+		setUserName: SetUserName,
+	},
 });
 
 const Navigation = createStaticNavigation(RootStack);
 
 export const AppRoutes = () => {
-   return <Navigation />;
-}
+	return <Navigation />;
+};
 
-export type TNavigationScreenProps = 
-    NativeStackNavigationProp<TScreenDefinitions>
+export type TNavigationScreenProps = NativeStackNavigationProp<TScreenDefinitions>;
 
-export type TRouteProps<T extends keyof TScreenDefinitions> = RouteProp<TScreenDefinitions, T>
+export type TRouteProps<T extends keyof TScreenDefinitions> = RouteProp<TScreenDefinitions, T>;
