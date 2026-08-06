36289554108f6501b17d08cd1f663d40f2d92357
ViniciusPilati <vini1718a@gmail.com>
2026-08-05 22:28:15 -0300

    feat: Configura fontes customizadas no aplicativo, além de evitar que a tela carregue antes das fontes serem carregadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { AppRoutes } from './Routes';
+import { Inter_400Regular, Inter_400Regular_Italic, Inter_800ExtraBold, useFonts } from "@expo-google-fonts/inter";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { AppRoutes } from "./Routes";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-      <AppRoutes />
-  );
+	const [loaded, error] = useFonts({
+		extraBold: Inter_800ExtraBold,
+		regularItalic: Inter_400Regular_Italic,
+		regular: Inter_400Regular,
+	});
+
+	useEffect(() => {
+		if (loaded || error) SplashScreen.hideAsync();
+	}, [loaded, error]);
+
+	if (!loaded && error) return null;
+
+	return <AppRoutes />;
 }
